05cfe1b8c95e76b8a8beec897103bbb0f559ed21
galaxyyvfx <ksgalaxyy@gmail.com>
2023-11-30 18:08:51 +0700

    Fixed tabs in /project

--- components/projects/details/index.js
+++ components/projects/details/index.js
@@ -1,7 +1,15 @@
 "use client";
 
 import PageContainer from "/components/container/PageContainer";
+import Tabs from "/components/shared/Tabs";
+
+import TabItems from "./tabItems";
 
 export default function ProjectDetails({ children }) {
-  return <PageContainer>{children}</PageContainer>;
+  return (
+    <PageContainer>
+      <Tabs uriPos={3} tabs={TabItems}></Tabs>
+      {children}
+    </PageContainer>
+  );
 }
--- components/projects/index.js
+++ components/projects/index.js
@@ -2,9 +2,6 @@
 
 import DashboardCard from "/components/shared/DashboardCard";
 import PageContainer from "/components/container/PageContainer";
-import Tabs from "/components/shared/Tabs";
-
-import TabItems from "./tabItems";
 
 const pageTitle = "Dự án";
 const pageDescription =
@@ -13,10 +10,7 @@ const pageDescription =
 export default function ProjectsPage({ children }) {
   return (
     <PageContainer title={pageTitle} description={pageDescription}>
-      <DashboardCard title={pageTitle}>
-        <Tabs uriPos={3} tabs={TabItems}></Tabs>
-        {children}
-      </DashboardCard>
+      <DashboardCard title={pageTitle}>{children}</DashboardCard>
     </PageContainer>
   );
 }
