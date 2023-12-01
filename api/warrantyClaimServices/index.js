4d23527af87b381b071c4377d1b6079b95ef5027
tkiet1808 <kietpttse161220@fpt.edu.vn>
2023-12-01 11:48:04 +0700

    add api function for project details

--- api/warrantyClaimServices/index.js
+++ api/warrantyClaimServices/index.js
@@ -1,13 +1,13 @@
-const getWarrantyClaimById = async (warrantyClaimId) => {
+const getAllWarrantyClaims = async () => {
     try {
         const response = await fetch(
-            `https://localhost:7062/api/WarrantyClaims/${warrantyClaimId}`,
+            `https://localhost:7062/api/WarrantyClaims`,
             { cache: 'no-store' }
         );
-        const warrantyClaim = await response.json();
-        return warrantyClaim;
+        const warrantyClaims = await response.json();
+        return warrantyClaims;
     } catch (error) {
-        console.error('Error fetching warranty claim by ID:', error);
+        console.error('Error fetching all warranty claims:', error);
         throw error;
     }
 };
@@ -26,4 +26,108 @@ const getWarrantyClaimsByProjectId = async (projectId) => {
     }
 };
 
-export { getWarrantyClaimById, getWarrantyClaimsByProjectId };
+const getWarrantyClaimsByUserId = async (userId) => {
+    try {
+        const response = await fetch(
+            `https://localhost:7062/api/WarrantyClaims/user/${userId}`,
+            { cache: 'no-store' }
+        );
+        const warrantyClaims = await response.json();
+        return warrantyClaims;
+    } catch (error) {
+        console.error('Error fetching warranty claims by user ID:', error);
+        throw error;
+    }
+};
+
+const getWarrantyClaimById = async (id) => {
+    try {
+        const response = await fetch(
+            `https://localhost:7062/api/WarrantyClaims/${id}`,
+            { cache: 'no-store' }
+        );
+        const warrantyClaim = await response.json();
+        return warrantyClaim;
+    } catch (error) {
+        console.error('Error fetching warranty claim by ID:', error);
+        throw error;
+    }
+};
+
+const createWarrantyClaim = async (request) => {
+    try {
+        const response = await fetch(
+            `https://localhost:7062/api/WarrantyClaims`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(request),
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('Create failed');
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching create warranty claim:', error);
+        throw error;
+    }
+};
+
+const updateWarrantyClaim = async (id, request) => {
+    try {
+        const response = await fetch(
+            `https://localhost:7062/api/WarrantyClaims/${id}`,
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(request),
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('Update failed');
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching update warranty claim:', error);
+        throw error;
+    }
+};
+
+const deleteWarrantyClaimById = async (id) => {
+    try {
+        const response = await fetch(
+            `https://localhost:7062/api/WarrantyClaims/${id}`,
+            {
+                method: 'DELETE',
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('Delete failed');
+        }
+
+        return true;
+    } catch (error) {
+        console.error('Error fetching delete warranty claim:', error);
+        throw error;
+    }
+};
+
+export {
+    getAllWarrantyClaims,
+    getWarrantyClaimsByProjectId,
+    getWarrantyClaimsByUserId,
+    getWarrantyClaimById,
+    createWarrantyClaim,
+    updateWarrantyClaim,
+    deleteWarrantyClaimById,
+};
