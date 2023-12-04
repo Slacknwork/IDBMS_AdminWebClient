750a56fe9dfbf25622e6644d76ec747f70c0e1e1
galaxyyvfx <ksgalaxyy@gmail.com>
2023-12-04 15:16:32 +0700

    Added redux persist & thunk

--- app/ClientLayout.js
+++ app/ClientLayout.js
@@ -1,20 +1,23 @@
 "use client";
 
-import { store } from "/store";
+import { store, persistor } from "/store";
 import { Provider } from "react-redux";
 import { baselightTheme } from "/utils/theme/DefaultColors";
 import { ThemeProvider } from "@mui/material/styles";
+import { PersistGate } from "redux-persist/integration/react";
 import { ToastContainer } from "react-toastify";
 import CssBaseline from "@mui/material/CssBaseline";
 
 export default function RootLayout({ children }) {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={baselightTheme}>
-        <CssBaseline />
-        {children}
-        <ToastContainer />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={baselightTheme}>
+          <CssBaseline />
+          {children}
+          <ToastContainer />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- store/reducers/rootReducers.js
+++ store/reducers/rootReducers.js
@@ -1,4 +1,4 @@
-import { combineReducers } from "redux";
+import { combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user";
 import breadcrumbReducer from "./breadcrumb";
 
