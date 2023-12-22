7c2948f90a28f30f0dbaf68ce25b8a9342657c62
tkiet1808 <kietpttse161220@fpt.edu.vn>
2023-12-22 23:03:14 +0700

    add tab item for project design

---
+++ app/(DashboardLayout)/system/project-designs/[id]/tabItems.js
@@ -0,0 +1,12 @@
+const items = [
+  {
+    path: "",
+    label: "Tổng quan",
+  },
+  {
+    path: "payment-stage-designs",
+    label: "Thiết kế giai đoạn thanh toán",
+  },
+];
+
+export default items;
