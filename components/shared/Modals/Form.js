655af3242d2887ba338b8f2a4f9ad682701f107c
tkiet1808 <kietpttse161220@fpt.edu.vn>
2023-12-29 22:39:09 +0700

    add disable close for form

--- components/shared/Modals/Form.js
+++ components/shared/Modals/Form.js
@@ -40,6 +40,7 @@ export default function FormModal({
   disableCloseOnSubmit,
   bottomLeftContent,
   size = "",
+  disabledClose
 }) {
   // STYLE
   const theme = useTheme();
@@ -54,8 +55,8 @@ export default function FormModal({
       size === "big" && isBigScreen
         ? "60rem"
         : size === "small"
-        ? "25rem"
-        : "40rem",
+          ? "25rem"
+          : "40rem",
     bgcolor: "background.paper",
     overflowY: "auto",
     boxShadow: 24,
@@ -68,7 +69,8 @@ export default function FormModal({
     setOpen(true);
   };
   const handleClose = () => {
-    setOpen(false);
+    if (disabledClose == false)
+      setOpen(false);
   };
   const handleSubmit = () => {
     !disableSubmitFunction && onSubmit();
