eaec6cb804a831499bd7ea61f9598e5f0d1d3081
tkiet1808 <kietpttse161220@fpt.edu.vn>
2023-12-31 21:22:03 +0700

    add avatar color and shortcut to util folder

--- components/shared/UserCard/index.js
+++ components/shared/UserCard/index.js
@@ -1,21 +1,7 @@
 "use client";
 
 import { Avatar, Box, Typography } from "@mui/material";
-import {
-  amber,
-  deepOrange,
-  orange,
-  teal,
-  indigo,
-  red,
-  blue,
-  pink,
-  green,
-  lime,
-  purple,
-  brown,
-  grey,
-} from "@mui/material/colors";
+import { getAvatarContent, getColorForAvatar } from "/utils/avatar"
 
 export default function UserCard({
   sx,
@@ -24,48 +10,6 @@ export default function UserCard({
   email = ".@mail.com",
   phone = "0123456789",
 }) {
-  // SET AVATAR LETTER
-  const getAvatarContent = (name) => {
-    if (name === "Không tìm thấy") return "?";
-
-    const words = name.split(" ");
-    const lastWord =
-      words.length > 0 ? words[words.length - 1].charAt(0).toUpperCase() : "";
-    const secondLastWord =
-      words.length > 1 ? words[words.length - 2].charAt(0).toUpperCase() : "";
-
-    return secondLastWord + lastWord;
-  };
-
-  //SET AVATAR COLOR
-  const getColorForAvatar = (name) => {
-    const avatarContent = getAvatarContent(name);
-
-    if (avatarContent === "?") return grey[900];
-
-    const colorSeed = avatarContent
-      .split("")
-      .map((char) => char.charCodeAt())
-      .reduce((sum, code) => sum + code, 0);
-
-    const colors = [
-      orange,
-      amber,
-      deepOrange,
-      teal,
-      indigo,
-      red,
-      blue,
-      pink,
-      green,
-      lime,
-      purple,
-      brown,
-    ];
-    const color = colors[colorSeed % colors.length][500];
-
-    return color || grey[900];
-  };
 
   return (
     <Typography variant="subtitle2">
