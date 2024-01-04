c8412d76f942e5c280f0e4a06c31e5793bf021aa
tkiet1808 <kietpttse161220@fpt.edu.vn>
2024-01-04 11:02:18 +0700

    update size color type

--- components/shared/Modals/ItemColors/CreateModal.js
+++ components/shared/Modals/ItemColors/CreateModal.js
@@ -164,7 +164,7 @@ export default function CreateItemModal() {
       </Grid>
 
       {/* COLOR TYPE */}
-      <Grid item xs={12} lg={6}>
+      <Grid item xs={12} lg={8}>
         <SelectForm
           title="Loại màu"
           required
