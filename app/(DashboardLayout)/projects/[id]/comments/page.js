b81bde6a44dd8fc6d847bd7a39171fa63ecad23d
galaxyyvfx <ksgalaxyy@gmail.com>
2024-01-10 22:26:50 +0700

    Displayed comments

--- app/(DashboardLayout)/projects/[id]/comments/page.js
+++ app/(DashboardLayout)/projects/[id]/comments/page.js
@@ -14,16 +14,29 @@ import {
   Typography,
 } from "@mui/material";
 import { useParams, useSearchParams } from "next/navigation";
+import { toast } from "react-toastify";
+import moment from "moment-timezone";
+import "moment/locale/vi";
 
 import { getCommentsByProjectId } from "/services/commentServices";
 
+import timezone from "/constants/timezone";
+
 import Search from "/components/shared/Search";
 import Pagination from "/components/shared/Pagination";
-import { toast } from "react-toastify";
 
-// Component for displaying comments
+import { getAvatarContent, getColorForAvatar } from "/utils/avatar";
+
+moment.tz.setDefault(timezone.momentDefault);
+moment.locale(timezone.momentLocale);
+
 export default function ProjectCommentsPage() {
   // CONSTANTS
+  const searchQuery = "search";
+  const pageQuery = "page";
+  const defaultPage = 1;
+  const pageSizeQuery = "pageSize";
+  const defaultPageSize = 5;
 
   // INIT
   const params = useParams();
@@ -35,22 +48,34 @@ export default function ProjectCommentsPage() {
   const [count, setCount] = useState(0);
 
   const fetchComments = async () => {
-    setLoading(true);
     try {
       const projectId = params.id;
-      const response = await getCommentsByProjectId({ projectId });
+      const search = searchParams.get(searchQuery) ?? "";
+      const page = searchParams.get(pageQuery) ?? defaultPage;
+      const pageSize = searchParams.get(pageSizeQuery) ?? defaultPageSize;
+
+      const response = await getCommentsByProjectId({
+        projectId,
+        search,
+        page,
+        pageSize,
+      });
       setComments(response.list);
       setCount(response.totalItem);
     } catch (error) {
       toast.error("Lỗi dữ liệu: Bình luận!");
-    } finally {
-      setLoading(false);
     }
   };
 
+  const fetchData = async () => {
+    setLoading(true);
+    await Promise.all([fetchComments()]);
+    setLoading(false);
+  };
+
   useEffect(() => {
-    fetchComments();
-  }, []);
+    fetchData();
+  }, [searchParams]);
 
   return (
     <Box sx={{ zIndex: 1 }}>
@@ -64,33 +89,38 @@ export default function ProjectCommentsPage() {
           comments.map((comment) => (
             <Box key={comment.id}>
               <ListItem alignItems="flex-start" sx={{ py: 2 }}>
-                <ListItemAvatar sx={{ my: "auto" }}>
-                  <Avatar alt="Remy Sharp">N</Avatar>
+                <ListItemAvatar
+                  color={getColorForAvatar(comment?.user?.name)}
+                  sx={{ my: "auto" }}
+                >
+                  <Avatar alt="Remy Sharp">
+                    {getAvatarContent(comment?.user?.name)}
+                  </Avatar>
                 </ListItemAvatar>
-                <Box sx={{ width: "40%", my: "auto" }}>
+                <Box sx={{ width: "50%", my: "auto" }}>
                   <Box sx={{ display: "flex" }}>
                     <Typography variant="h6" sx={{ whiteSpace: "nowrap" }}>
-                      Thủy Ngọc Toàn
+                      {comment?.user?.name ?? ""}
                     </Typography>
                     <Typography variant="subtitle2" sx={{ mx: 1 }}>
                       -
                     </Typography>
-                    <Typography variant="p">17/02/2023</Typography>
+                    <Typography variant="p">
+                      {comment?.createdTime &&
+                        moment(comment?.createdTime).format("lll")}
+                    </Typography>
                   </Box>
                   <Typography
                     variant="subtitle2"
                     textAlign="justify"
                     sx={{ pr: 3 }}
                   >
-                    Chỉnh giúp anh cái vách màu trắng nhéChỉnh giúp anh cái vách
-                    màu trắng nhéChỉnh giúp anh cái vách màu trắng nhéChỉnh giúp
-                    anh cái vách màu trắng nhéChỉnh giúp anh cái vách màu trắng
-                    nhéChỉnh giúp anh cái vách màu trắng nhé
+                    {comment?.content ?? "N/A"}
                   </Typography>
                 </Box>
                 <Box
                   sx={{
-                    width: "30%",
+                    width: "25%",
                     my: "auto",
                     display: "flex",
                     borderLeft: 1,
@@ -120,7 +150,7 @@ export default function ProjectCommentsPage() {
                 </Box>
                 <Box
                   sx={{
-                    width: "30%",
+                    width: "25%",
                     my: "auto",
                     display: "flex",
                     borderLeft: 1,
