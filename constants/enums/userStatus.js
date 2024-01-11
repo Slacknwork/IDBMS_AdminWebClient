06b4a09b3e181ac77902d9a5cd013c35185e1a5e
tkiet1808 <kietpttse161220@fpt.edu.vn>
2024-01-11 20:01:16 +0700

    remove user status locked

--- constants/enums/userStatus.js
+++ constants/enums/userStatus.js
@@ -2,21 +2,18 @@ const userStatusIndex = {
   Active: 0,
   Unverified: 1,
   Suspended: 2,
-  Locked: 3,
 };
 
 const userStatusEnglishOptions = [
   "Active",
   "Unverified",
   "Suspended",
-  "Locked",
 ];
 
 const userStatusOptions = [
   "Đang hoạt động",
   "Chưa xác nhận",
   "Bị kiểm soát",
-  "Khóa",
 ];
 
 export { userStatusIndex, userStatusEnglishOptions };
